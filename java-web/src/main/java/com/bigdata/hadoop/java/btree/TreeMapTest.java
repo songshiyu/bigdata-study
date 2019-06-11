b97e948bf4c5bcf7c91ca299d004425f63ad968e
user <111>
2019-06-11 16:11:31 +0800

    添加spark-streaming学习模块

---
+++ java-web/src/main/java/com/bigdata/hadoop/java/btree/TreeMapTest.java
@@ -0,0 +1,16 @@
+package com.bigdata.hadoop.java.btree;
+
+/**
+ * @author songshiyu
+ * @date 2019/6/10 15:03
+ */
+public class TreeMapTest {
+
+    public static void main(String[] args) {
+        Integer a = 3;
+        Integer b = 2;
+        int i = a.compareTo(b);
+        System.out.println(i);
+
+    }
+}
