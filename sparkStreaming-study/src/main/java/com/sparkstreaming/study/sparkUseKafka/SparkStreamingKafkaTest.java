2adf7b33022f84598d0ff2c30c3383e16e56a394
user <111>
2019-06-14 17:35:53 +0800

    添加spark-streaming学习模块

---
+++ sparkStreaming-study/src/main/java/com/sparkstreaming/study/sparkUseKafka/SparkStreamingKafkaTest.java
@@ -0,0 +1,13 @@
+package com.sparkstreaming.study.sparkUseKafka;
+
+
+/**
+ * @author songshiyu
+ * @date 2019/6/14 16:33
+ */
+public class SparkStreamingKafkaTest {
+
+    public static void main(String[] args) {
+
+    }
+}
